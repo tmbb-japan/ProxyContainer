143c70330f6493f957cf8dfce9615a23d1d3f407
tmbb-japan <woo787@naver.com>
2024-12-12 15:31:03 +0900

    fix : logrotate error: getting file ACL

--- logrotate/upload_to_r2.sh
+++ logrotate/upload_to_r2.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# Set bucket name and path
-R2_BUCKET="your-r2-bucket"
-R2_PATH="logs/"
+# Load environment variables
+source /etc/environment
 
 # Specify files to upload
 FILE_TO_UPLOAD=\$1
 
 # Upload a File to R2 Using AWS CLI
-aws s3 cp $FILE_TO_UPLOAD s3://$R2_BUCKET/$R2_PATH
+aws s3 cp $FILE_TO_UPLOAD s3://$R2_BUCKET/$R2_PATH --endpoint-url $R2_ENDPOINT_URL
+
+# Log file for upload status
+LOGFILE="/var/log/nginx/upload_to_r2.log"
+
+# Check the exit status of the upload command
+if [ $? -ne 0 ]; then
+    # Log the failure
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - Failed to upload $FILE_TO_UPLOAD" >> "$LOGFILE"
+    exit 1
+else
+    # Log the success
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - Successfully uploaded $FILE_TO_UPLOAD" >> "$LOGFILE"
+fi
